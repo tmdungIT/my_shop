d31f4b60735ddcdbb5abbe3759233889c802f053
tmdungit <tmdung2813@gmail.com>
2023-11-08 21:51:39 +0700

    add

--- src/component/site/Footer/Footer.jsx
+++ src/component/site/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 function Footer(props) {
     return (
         <div>
-            <h1>Footer</h1>
+            <h1>Footer sdryhrtherthertherthrthrtjrtyj</h1>
+            <h1>Footer sdryhrtherthertherthrthrtjrtyj</h1>
+            <h1>Footer sdryhrtherthertherthrthrtjrtyj</h1>
+            <h1>Footer sdryhrtherthertherthrthrtjrtyj</h1>
+            <h1>Footer sdryhrtherthertherthrthrtjrtyj</h1>
         </div>
     );
 }
